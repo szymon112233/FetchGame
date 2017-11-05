73a73aaaa17cba23f0b89086972693e4e6b0d4b7
Szymon Piórkowski <szymmon123@gmail.com>
2017-11-05 20:56:04 +0100

    AddedAdded basic enemies, added shooting

---
+++ Assets/Scripts/Autodestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Autodestroy : MonoBehaviour {
+
+    public float autodestroyTime = 2.0f;
+	
+	void Update () {
+        if (autodestroyTime < 0.0f)
+            Destroy(gameObject);
+        autodestroyTime -= Time.deltaTime;
+	}
+}
--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class PlayerMovment : MonoBehaviour {
 
     public float speed = 10;
+    public float bulletForce = 10;
+
+    public float shootInterval = 0.5f;
+    public float shootTimer = 0.0f;
+
 
     public Transform targetTransform;
+    public Transform gunPointTransform;
     public Rigidbody rigidBody;
-
+    public GameObject bulletPrefab;
 
     void Update () {
+        UpdateTimers();
         UpdateInput();
-	}
+    }
 
     private void UpdateInput()
     {
@@ -23,12 +30,37 @@ public class PlayerMovment : MonoBehaviour {
 
         rigidBody.MovePosition(rigidBody.position + translateVector);
 
-        if (targetTransform != null)
+        if (targetTransform != null && gunPointTransform != null)
         {
-            rigidBody.rotation = Quaternion.LookRotation(new Vector3(targetTransform.position.x, rigidBody.position.y, targetTransform.position.z) - rigidBody.position, Vector3.up);
-            Debug.DrawLine(rigidBody.position, new Vector3(targetTransform.position.x, rigidBody.position.y, targetTransform.position.z));
+            rigidBody.rotation = Quaternion.LookRotation(new Vector3(targetTransform.position.x, gunPointTransform.position.y, targetTransform.position.z) - gunPointTransform.position, Vector3.up);
+            Debug.DrawLine(gunPointTransform.position, new Vector3(targetTransform.position.x, rigidBody.position.y, targetTransform.position.z));
         }
-            
+        
 
+        if (Input.GetButton("Fire1"))
+        {
+            if (shootTimer <= 0.0f)
+                Shoot(translateVector);
+        }
+
+    }
+
+    private void UpdateTimers()
+    {
+        if (shootTimer > 0.0f)
+            shootTimer -= Time.deltaTime;
+    }
+
+    private void Shoot(Vector3 moveVector)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, gunPointTransform.position, rigidBody.rotation) as GameObject;
+        if (bullet != null)
+        {
+            Vector3 forceVector = new Vector3(targetTransform.position.x, rigidBody.position.y, targetTransform.position.z) - gunPointTransform.position + moveVector;
+            forceVector.Normalize();
+            forceVector *= bulletForce;
+            bullet.GetComponent<Rigidbody>().AddForce(forceVector, ForceMode.Impulse);
+            shootTimer = shootInterval;
+        }
     }
 }
